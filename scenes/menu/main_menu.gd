de8711a2326d26b3440ca44573f822b67be5324f
Edi <ediloupatty@gmail.com>
2026-03-08 18:04:52 +0800

    add scenes

--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -10,3 +10,7 @@ func _on_level_1_pressed() -> void:
 
 func _on_level_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/level2.tscn")
+
+
+func _on_main_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/level1.tscn")
